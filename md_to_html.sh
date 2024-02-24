5f5d2e5793afe3930c3868dcdd80416c2d8a0c07
John Christensen <johnedchristensen@gmail.com>
2024-02-23 18:01:52 -0600

    Added images

--- md_to_html.sh
+++ md_to_html.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-pandoc --css=style.css -s -f markdown --metadata pagetitle="John Christensen" --to=html5 README.md -o index.html
+#npm isntall -g liverserver
+live-server &
+
+convert_to_html() {
+    pandoc --css=style.css -s -f markdown --metadata pagetitle="John Christensen" --to=html5 README.md -o index.html
+    echo "Conversion complete. HTML updated."
+}
+
+# Initial conversion
+convert_to_html
+
+# Watch for modifications in README.md and style.css
+while true; do
+    inotifywait -e modify README.md style.css
+    convert_to_html
+done
